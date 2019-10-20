da119aa900417bfc21dfb49ecd0f281a04f02bf2
Zoltán Mizsei <zmizsei@extrowerk.com>
2019-10-20 19:23:35 +0200

    Initial commit

---
+++ src/beeblio.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018, Your Name <your@email.address>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef _H_BEEBLIO
+#define _H_BEEBLIO
+
+
+#include <SupportDefs.h>
+
+
+//class {
+//public:
+//
+//private:
+//
+//};
+
+
+#endif // _H_BEEBLIO
